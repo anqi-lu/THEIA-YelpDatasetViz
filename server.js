1538e72fc23844479cc0cacbd0f716ea5184f397
ziyanding <zding@wpi.edu>
2016-11-20 13:48:05 -0500

    Simple website page for proposal

---
+++ server.js
@@ -0,0 +1,33 @@
+var http = require('http')
+  , fs   = require('fs')
+  , url  = require('url')
+  , port = 8080;
+
+var server = http.createServer (function (req, res) {
+  var uri = url.parse(req.url)
+
+  switch( uri.pathname ) {
+    case '/':
+      sendFile(res, 'proposal.txt', 'text')
+      break
+    case '/index.html':
+      sendFile(res, 'proposal.txt', 'text')
+      break
+    default:
+      res.end('404 not found')
+  }
+})
+
+server.listen(process.env.PORT || port);
+console.log('listening on 8080')
+
+// subroutines
+
+function sendFile(res, filename, contentType) {
+  contentType = contentType || 'text/html'
+  fs.readFile(filename, function(error, content) {
+    res.writeHead(200, {'Content-type': contentType})
+    res.end(content, 'utf-8')
+  })
+
+}

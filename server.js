6df2d1bbf9927c99c4d7e5904f14eb711adf343f
augustlu <alu213000@gmail.com>
2016-11-21 08:59:45 -0500

    solve merge conflict

--- server.js
+++ server.js
@@ -8,17 +8,10 @@ var server = http.createServer (function (req, res) {
 
   switch( uri.pathname ) {
     case '/':
-<<<<<<< HEAD
       sendFile(res, 'index.html')
       break
     case '/index.html':
       sendFile(res, 'index.html')
-=======
-      sendFile(res, 'proposal.txt', 'text')
-      break
-    case '/index.html':
-      sendFile(res, 'proposal.txt', 'text')
->>>>>>> 1538e72fc23844479cc0cacbd0f716ea5184f397
       break
     default:
       res.end('404 not found')
@@ -30,17 +23,10 @@ console.log('listening on 8080')
 
 // subroutines
 
-<<<<<<< HEAD
-function sendFile(res, filename) {
-
-  fs.readFile(filename, function(error, content) {
-    res.writeHead(200, {'Content-type': 'text/html'})
-=======
 function sendFile(res, filename, contentType) {
   contentType = contentType || 'text/html'
   fs.readFile(filename, function(error, content) {
     res.writeHead(200, {'Content-type': contentType})
->>>>>>> 1538e72fc23844479cc0cacbd0f716ea5184f397
     res.end(content, 'utf-8')
   })
 

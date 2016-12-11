1783239042128ac936c9c055133e0665ae220f21
ziyanding <zding@wpi.edu>
2016-12-11 14:20:43 -0500

    add yelp api

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 var http = require('http')
   , fs   = require('fs')
   , url  = require('url')
+  , Yelp = require('yelpv3')
   , port = 8080;
+
+var yelp = new Yelp({
+  app_id: "OVW6i4q-Pn01w3btb-7Aaw",
+  app_secret: "2Z111pczUu6xlQCV3g3aEfr9XVPoeCiy239jjmyASb1bzRqA1KhCfKU4dni5S86g"
+});
+
 
 var server = http.createServer (function (req, res) {
   var uri = url.parse(req.url)
 
   switch( uri.pathname ) {
     case '/':
-      sendFile(res, 'index.html')
+      sendFile(res, 'index.html')    
       break
     case '/index.html':
       sendFile(res, 'index.html')
@@ -17,15 +24,20 @@ var server = http.createServer (function (req, res) {
       sendFile(res, 'treemap.html')
       break
     case '/categories.json':
-      sendFile(res, 'categories.json')
+      sendFile(res, 'json/categories.json')
       break
     case '/zoomableTM.html':
       sendFile(res, 'zoomableTM.html')
       break
     case '/bus_cat.json':
-      sendFile(res, 'bus_cat.json')
+      sendFile(res, 'json/bus_cat.json')
       break
-     
+    case '/test.html':
+      sendFile(res, 'test.html')
+      break 
+    case '/table.html':
+      res.end(JSON.stringify(table))
+      break;
     default:
       res.end('404 not found')
   }
@@ -41,5 +53,7 @@ function sendFile(res, filename, contentType) {
     res.writeHead(200, {'Content-type': contentType})
     res.end(content, 'utf-8')
   })
-
 }
+
+
+

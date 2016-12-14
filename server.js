74b27e996d4d4d1f506bcaca7f98f97216814079
ziyanding <zding@wpi.edu>
2016-12-13 21:33:15 -0500

    change server, add table.html

--- server.js
+++ server.js
@@ -9,16 +9,15 @@ var app = express();
 var bodyParser = require('body-parser')
 
 //app.use(express.bodyParser());
-
 var yelp = new Yelp({
   app_id: "OVW6i4q-Pn01w3btb-7Aaw",
   app_secret: "2Z111pczUu6xlQCV3g3aEfr9XVPoeCiy239jjmyASb1bzRqA1KhCfKU4dni5S86g"
 });
 
+
 // get all the files in public and json folder
 app.use(express.static('public'));
 app.use(express.static('json'));
-
 
 // respond to a post request
 app.post('/', function (req, res) {
@@ -26,6 +25,59 @@ app.post('/', function (req, res) {
   res.send('Got a POST request')
 });
 
+app.post('/search', function(req, res){
+  search(req, res);
+})
+
+app.post('/getInfo', function(req, res){
+  getInfo(req, res);
+})
+
 app.listen(process.env.PORT || port, function() {
   console.log("listening on 8080!");
 });
+
+//function to handle requests from table.html
+function search(req, res){
+    yelp.search({term:'food', location:'Carlifornia', limit: 10})
+        .then(function (data) { 
+            //console.log(data);
+            var output = JSON.parse(data);
+            //console.log(output);
+            console.log(output.total)
+            res.end(JSON.stringify(output))
+        })
+        .catch(function (err) { console.error(err);});
+}
+
+function getSingleBus(id){
+    return new Promise(function(resolve, reject){
+        yelp.business(id).then(function(data){
+                temp = JSON.parse(data);
+                resolve(temp)
+            })
+         .catch(function (err){
+            console.error(err);
+            reject(err)})
+    })
+}
+
+function getInfo(req, res){
+    var chunk = ""
+    req.on('data', function(data) {
+        chunk += data;
+    })
+    req.on('end', function(data) {
+        //console.log(chunk)
+        var obj = chunk.split(",")
+        var busInfoPromise = obj.map(getSingleBus)
+        Promise.all(busInfoPromise).then(function(businfo){
+            //console.log(businfo)
+            res.end(JSON.stringify(businfo))
+        }).catch(function(businfo){
+            console.log("Error Occurs")
+        })
+    }) 
+}
+
+
